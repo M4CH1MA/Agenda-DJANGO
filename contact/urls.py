8a2f78603f090b63f8bc1284d64f32bef4a29cf6
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-03-07 21:25:02 -0300

    Aulas DJANGO Projeto Agenda

---
+++ contact/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from contact import views
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('search/', views.search, name='search'),
+    path('', views.index, name='index'),
+    
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+]
