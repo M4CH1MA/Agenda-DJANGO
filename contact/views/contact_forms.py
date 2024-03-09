d90e52f260dceafbb77e62eff3453af19709b97b
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-03-09 15:51:38 -0300

    form

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from contact.models import Contact
-from django.core.paginator import Paginator
+from django.shortcuts import render
+from contact.forms import ContactForm
+
 
 
 # Create your views here.
 def create(request):
 
-    context = {}
+    if request.method == 'POST':
+        #print(request.POST.get("first_name"))
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
+    #method GET
+    context = {
+            'form': ContactForm()
+        }
 
     return render(
         request,
         'contact/create.html',
         context
     )
+
+    
