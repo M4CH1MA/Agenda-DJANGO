61fc247ea4f5e9fc720644787760c6a5034be95d
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-03-10 09:47:29 -0300

    Create and Update

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,30 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = models.Contact
-        fields = 'first_name', 'last_name', 'phone'
+        fields = 'first_name', 'last_name', 'phone', 'email', 'description', 'category'
+
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'placeholder':'Digite seu nome',
+                } 
+            )
+        }
+
+        def clean(self):
+            return super().clean()
+        
+        #def clean_first_name(self):
+        #   first_name = self.cleaned_data.get('first_name')
+
+        #   if first_name == 'ABC':
+        #       self.add_error('first_name',ValidationError('Veio do add_error',code='invalid'))
+
+        #   return first_name
+    
--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
     
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
-    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'), #Read (ler o contato informacoes sobre)
+    path('contact/create/', views.create, name='create'),  #Create
+    path('contact/<int:contact_id>/update/', views.update, name='update'),  #Update
 ]
